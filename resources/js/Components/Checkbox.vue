496aa03b044bea51a9d3da908f27107e917a9cdd
szabilukacs <64462503+szabilukacs@users.noreply.github.com>
2023-03-01 20:51:11 +0200

    some changes

--- resources/js/Components/Checkbox.vue
+++ resources/js/Components/Checkbox.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { computed } from 'vue';
+import { computed } from 'vue'
 
-const emit = defineEmits(['update:checked']);
+const emit = defineEmits(['update:checked'])
 
 const props = defineProps({
     checked: {
@@ -11,24 +11,24 @@ const props = defineProps({
     value: {
         default: null,
     },
-});
+})
 
 const proxyChecked = computed({
     get() {
-        return props.checked;
+        return props.checked
     },
 
     set(val) {
-        emit('update:checked', val);
+        emit('update:checked', val)
     },
-});
+})
 </script>
 
 <template>
     <input
+        v-model="proxyChecked"
         type="checkbox"
         :value="value"
-        v-model="proxyChecked"
         class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
-    />
+    >
 </template>
--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, onMounted, onUnmounted, ref } from 'vue';
+import { computed, onMounted, onUnmounted, ref } from 'vue'
 
 const props = defineProps({
     align: {
@@ -11,34 +11,34 @@ const props = defineProps({
     contentClasses: {
         default: () => ['py-1', 'bg-white'],
     },
-});
+})
 
 const closeOnEscape = (e) => {
     if (open.value && e.key === 'Escape') {
-        open.value = false;
+        open.value = false
     }
-};
+}
 
-onMounted(() => document.addEventListener('keydown', closeOnEscape));
-onUnmounted(() => document.removeEventListener('keydown', closeOnEscape));
+onMounted(() => document.addEventListener('keydown', closeOnEscape))
+onUnmounted(() => document.removeEventListener('keydown', closeOnEscape))
 
 const widthClass = computed(() => {
     return {
         48: 'w-48',
-    }[props.width.toString()];
-});
+    }[props.width.toString()]
+})
 
 const alignmentClasses = computed(() => {
     if (props.align === 'left') {
-        return 'origin-top-left left-0';
+        return 'origin-top-left left-0'
     } else if (props.align === 'right') {
-        return 'origin-top-right right-0';
+        return 'origin-top-right right-0'
     } else {
-        return 'origin-top';
+        return 'origin-top'
     }
-});
+})
 
-const open = ref(false);
+const open = ref(false)
 </script>
 
 <template>
@@ -48,7 +48,11 @@ const open = ref(false);
         </div>
 
         <!-- Full Screen Dropdown Overlay -->
-        <div v-show="open" class="fixed inset-0 z-40" @click="open = false"></div>
+        <div
+            v-show="open"
+            class="fixed inset-0 z-40"
+            @click="open = false"
+        />
 
         <transition
             enter-active-class="transition ease-out duration-200"
@@ -65,7 +69,10 @@ const open = ref(false);
                 style="display: none"
                 @click="open = false"
             >
-                <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
+                <div
+                    class="rounded-md ring-1 ring-black ring-opacity-5"
+                    :class="contentClasses"
+                >
                     <slot name="content" />
                 </div>
             </div>

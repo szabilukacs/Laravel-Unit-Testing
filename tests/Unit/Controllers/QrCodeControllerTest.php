68e8592dfaa992418a537d3e80811c48cd6ddc3e
szabilukacs <64462503+szabilukacs@users.noreply.github.com>
2023-03-07 21:51:00 +0200

    Add QrCode Test

--- tests/Unit/Controllers/QrCodeControllerTest.php
+++ tests/Unit/Controllers/QrCodeControllerTest.php
@@ -2,17 +2,50 @@
 
 namespace Tests\Unit\Controllers;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class QrCodeControllerTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+
+    public function test_a_qrCode_view_can_be_rendered(): void
     {
-        $this->assertTrue(true);
+        $view = $this->view('qrcode.index');
+
+        $view->assertSee('Generate a QR code');
+        $view->assertSee('Simple QR Code');
+    }
+
+    public function test_a_qrCode_show_view_can_be_rendered(): void
+    {
+        $view = $this->view('qrcode.show', ['url' => 'https://ms.sapientia.ro/hu']);
+
+        $view->assertSee('Your QR Code');
+    }
+
+    public function test_function_generate_empty_url(): void
+    {
+        $empyt_url = '';
+
+        $response = $this->post('qrcode', ['url' => $empyt_url]);
+        $response->assertStatus(200);
+        $response->assertSee('Generate a QR code');
+    }
+
+    public function test_function_generate_normal_url(): void
+    {
+        $url = 'https://ms.sapientia.ro/hu';
+
+        $response = $this->post('qrcode', ['url' => $url]);
+        $response->assertStatus(200);
+        $response->assertSee('Your QR Code');
+    }
+
+    public function test_function_can_export_url(): void
+    {
+        $url = 'ms.sapientia.ro';
+
+        $response = $this->get('qrcode/export/'.$url);
+        $response->assertStatus(200);
+        $response->assertSee('The QR code has been exported successfully!');
     }
 }
